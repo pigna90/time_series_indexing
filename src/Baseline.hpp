de2949d76bc99d3740e8ec24f9b99a706dc64da5
smoldino <smoldino@gmail.com>
2016-10-22 12:51:35 +0200

    Adjusting new functions in the class body.

--- src/Baseline.hpp
+++ src/Baseline.hpp
@@ -6,21 +6,21 @@
 #include <queue>
 #include <string>
 #include <vector>
-/*#include <cereal/archives/binary.hpp>
+#include <cereal/archives/binary.hpp>
 #include <cereal/types/map.hpp>
 #include <cereal/types/string.hpp>
-#include <cereal/types/vector.hpp>*/
+#include <cereal/types/vector.hpp>
 
 class Baseline {
 private:
 	std::map<std::string, std::vector<uint32_t>> m_time_series;
 	std::vector<uint16_t> m_dates;
 
-	/*template <class Archive>
+	template <class Archive>
 	void TimeSeries::serialize(Archive &archive) {
 		archive(m_time_series);
-	}*/
-	
+	}
+
 public:
 	Baseline() {}
 	~Baseline() {}
@@ -32,7 +32,7 @@ public:
 		  * parseLine, chiamata da populateDataStructure*/
 	}
 
-	/*void serialize_data(const string &file_name) const {
+	void serialize_data(const string &file_name) const {
 		std::ofstream os(file_name, std::ios::binary);                    
 		cereal::BinaryOutputArchive output(os);
 		output(m_time_series);
@@ -42,19 +42,20 @@ public:
 		std::ifstream is(file_name, std::ios::binary);
 		cereal::BinaryInputArchive input(is);
 		input(m_time_series);
-	}*/
+	}
 
-	/*inline std::vector<uint32_t> range(
+	inline std::vector<uint32_t> range(
 		const std::string &page, uint32_t time1, uint32_t time2) const {
 		
 		const auto lt_idx = std::lower_bound(m_dates.begin(), m_dates.end(), time1);
 		const auto rt_idx = std::lower_bound(lt_idx, m_dates.end(), time2);		
 		const auto pg = m_time_series.at(page);
 		
-		std::vector<uint32_t> result(rt_idx - lt_idx);
-		
 		const auto lt_end = lt_idx - m_dates.begin();
 		const auto rt_end = rt_idx - m_dates.begin();
+		
+		std::vector<uint32_t> result;
+		result.reserve(rt_end - lt_end + 1);
 		
 		for(size_t idx = lt_end; idx <= rt_end; ++idx)
 			result.push_back(pg[idx]);
@@ -69,10 +70,10 @@ public:
 		const auto rt_idx = std::lower_bound(lt_idx, m_dates.end(), time2);
 		const auto pg = m_time_series.at(page);
 		
-		std::priority_queue<std::pair<uint16_t, uint32_t>> heap;
-		
 		const auto lt_end = lt_idx - m_dates.begin();
 		const auto rt_end = rt_idx - m_dates.begin();
+		
+		std::priority_queue<std::pair<uint16_t, uint32_t>> heap;
 		
 		for(size_t idx = lt_end; idx <= rt_end; ++idx) {
 			if(heap.size() < k)
@@ -92,7 +93,7 @@ public:
 		}
 		
 		return result;
-	}*/
+	}
 	
 	size_t size() const {
 		std::vector<std::vector<uint32_t>> data;
