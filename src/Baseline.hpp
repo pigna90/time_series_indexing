32163cdf6d1b073d2f549ccf3aed92c76ef6eace
Alessandro <alerom90@gmail.com>
2016-10-27 16:11:42 +0200

    Commit for merge

--- src/Baseline.hpp
+++ src/Baseline.hpp
@@ -9,27 +9,20 @@
 #include <set>
 #include <string>
 #include <vector>
-/*#include <cereal/archives/binary.hpp>
+#include <cereal/archives/binary.hpp>
 #include <cereal/types/map.hpp>
 #include <cereal/types/string.hpp>
-#include <cereal/types/vector.hpp>*/
-
-class Compare {
-public:
-	bool operator()(const std::pair<uint32_t, uint32_t> &p1, const std::pair<uint32_t, uint32_t> &p2) {
-		return p1.second > p2.second;
-	}
-};
+#include <cereal/types/vector.hpp>
 
 class Baseline {
 private:
 	std::map<std::string, std::vector<uint32_t>> m_time_series;
 	std::vector<uint32_t> m_dates;
 
-	/*template <class Archive>
+	template <class Archive>
 	void serialize(Archive &archive) {
 		archive(m_time_series,m_dates);
-	}*/
+	}
 
 public:
 	Baseline() {}
@@ -39,7 +32,7 @@ public:
 		std::set<std::string> lines;
 		std::ifstream infile(dataset_name);
 		std::string token;
-		
+
 		for(std::string line; getline(infile, line); ) {
 			lines.insert(line);
 			token = line.substr(0, line.find("\t"));
@@ -47,9 +40,8 @@ public:
 			if(std::find(m_dates.begin(), m_dates.end(), date) == m_dates.end())
 				m_dates.push_back(date);
 		}
-		std::sort(m_dates.begin(), m_dates.end());
 		infile.close();
-		
+
 		for(auto line: lines){
 			uint32_t date, count;
 			std::string page, token;
@@ -62,7 +54,7 @@ public:
 			page = token.substr(0, token.find("\t"));
 			//Parse page counter
 			count = atoi((token.substr(token.find("\t")+1, token.length())).c_str());
-			
+
 			if (m_time_series.find(page) == m_time_series.end()) {
 				std::vector<uint32_t> visits(m_dates.size(),0);
 				m_time_series.insert(make_pair(page,visits));
@@ -72,55 +64,49 @@ public:
 		}
 	}
 
-	/*void serialize_data(const std::string &file_name) const {
-		std::ofstream os(file_name, std::ios::binary);                    
+	void serialize_data(const std::string &file_name) const {
+		std::ofstream os(file_name, std::ios::binary);
 		cereal::BinaryOutputArchive output(os);
 		output(m_time_series,m_dates);
 	}
-		
+
 	void load_data(const std::string &file_name) {
 		std::ifstream is(file_name, std::ios::binary);
 		cereal::BinaryInputArchive input(is);
 		input(m_time_series,m_dates);
-	}*/
+	}
 
 	inline std::vector<uint32_t> range(
 		const std::string &page, uint32_t time1, uint32_t time2) const {
-		
-		const auto lt_idx = std::lower_bound(m_dates.begin(), m_dates.end(), time1);
-		const auto rt_idx = std::lower_bound(lt_idx, m_dates.end(), time2);		
-		const auto pg = m_time_series.at(page);
-		
-		const auto lt_end = lt_idx - m_dates.begin();
-		const auto rt_end = rt_idx - m_dates.begin();
-		
-		std::vector<uint32_t> result;
-		result.reserve(rt_end - lt_end + 1);
-		
-		for(size_t idx = lt_end; idx <= rt_end; ++idx)
-			result.push_back(pg[idx]);
-			
-		return result;
-	}
-	
-	inline std::vector<std::pair<uint32_t, uint32_t>> rangeTopK(
-		const std::string &page, uint32_t time1, uint32_t time2, uint32_t k) const {
-			
+
 		const auto lt_idx = std::lower_bound(m_dates.begin(), m_dates.end(), time1);
 		const auto rt_idx = std::lower_bound(lt_idx, m_dates.end(), time2);
 		const auto pg = m_time_series.at(page);
-		
+
 		const auto lt_end = lt_idx - m_dates.begin();
 		const auto rt_end = rt_idx - m_dates.begin();
-		
-		std::priority_queue<std::pair<uint32_t, uint32_t>,
-							std::vector<std::pair<uint32_t, uint32_t>>,
-							/*[](const std::pair<uint32_t, uint32_t> &p1, 
-							   const std::pair<uint32_t, uint32_t> &p2) -> bool {
-								return p1.second > p2.second;
-							}> heap;*/
-							Compare> heap;
-		
+
+		std::vector<uint32_t> result;
+		result.reserve(rt_end - lt_end + 1);
+
+		for(size_t idx = lt_end; idx <= rt_end; ++idx)
+			result.push_back(pg[idx]);
+
+		return result;
+	}
+
+	inline std::vector<std::pair<uint32_t, uint32_t>> rangeTopK(
+		const std::string &page, uint32_t time1, uint32_t time2, uint32_t k) const {
+
+		const auto lt_idx = std::lower_bound(m_dates.begin(), m_dates.end(), time1);
+		const auto rt_idx = std::lower_bound(lt_idx, m_dates.end(), time2);
+		const auto pg = m_time_series.at(page);
+
+		const auto lt_end = lt_idx - m_dates.begin();
+		const auto rt_end = rt_idx - m_dates.begin();
+
+		std::priority_queue<std::pair<uint32_t, uint32_t>> heap;
+
 		for(size_t idx = lt_end; idx <= rt_end; ++idx) {
 			if(heap.size() < k)
 				heap.emplace(m_dates[idx], pg[idx]);
@@ -129,27 +115,26 @@ public:
 				heap.pop();
 			}
 		}
-		
+
 		std::vector<std::pair<uint32_t, uint32_t>> result;
-		
 		result.reserve(heap.size());
-		
+
 		for(size_t idx = 0; idx < heap.size(); ++idx) {
 			result.push_back(heap.top());
 			heap.pop();
 		}
-		
+
 		return result;
 	}
-	
+
 	size_t size() const {
 		std::vector<std::pair<std::string, std::vector<uint32_t>>> values;
-		
+
 		for(auto idx = m_time_series.begin(); idx != m_time_series.end(); ++idx) {
 			values.emplace_back(idx->first, idx->second);
 		}
-		
-		size_t result = sizeof(std::string) * sizeof(std::vector<uint32_t>) * values.size();  
+
+		size_t result = sizeof(std::string) * sizeof(std::vector<uint32_t>) * values.size();
 		return result;
 	}
 
