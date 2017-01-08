e697ad0d8a93611d6b1d04a76678db6a0c011367
Alessandro <alerom90@gmail.com>
2017-01-08 12:25:49 +0100

    Added map for dates index lookup

--- src/Baseline.hpp
+++ src/Baseline.hpp
@@ -18,12 +18,14 @@ class Baseline {
 private:
 	//Main data structure for time series
 	std::map<std::string, std::vector<uint32_t>> m_time_series;
-	//List of dates
+	//Map of dates for lookup
+	std::map<uint32_t,size_t> m_map_dates;
+	//Vector of dates
 	std::vector<uint32_t> m_dates;
 
 	template <class Archive>
 	void serialize(Archive &archive) {
-		archive(m_time_series,m_dates);
+		archive(m_time_series, m_map_dates, m_dates);
 	}
 
 public:
@@ -39,6 +41,7 @@ public:
 		std::ifstream infile(dataset_name);
 		std::string token;
 
+		//Reads all dates available on data
 		for(std::string line; getline(infile, line); ) {
 			lines.push_back(line);
 			token = line.substr(0, line.find("\t"));
@@ -51,6 +54,13 @@ public:
 		std::sort(m_dates.begin(), m_dates.end());
 		m_dates.erase( unique( m_dates.begin(), m_dates.end() ), m_dates.end());
 
+		//HashTable for retrieve lookup index position instead
+		//of find it from vector
+		size_t value = 0;
+		for(auto d: m_dates){
+			m_map_dates.insert(std::make_pair(d,value));
+			value++;
+		}
 
 		for(auto line: lines){
 			uint32_t date, count;
@@ -69,8 +79,7 @@ public:
 				std::vector<uint32_t> visits(m_dates.size(),0);
 				m_time_series.insert(make_pair(page,visits));
 			}
-			size_t date_idx = std::find(m_dates.begin(),m_dates.end(),date) - m_dates.begin();
-			(m_time_series[page])[date_idx] = count;
+			(m_time_series[page])[m_map_dates[date]] = count;
 		}
 	}
 
@@ -94,7 +103,7 @@ public:
 	void serialize_data(const std::string &file_name) const {
 		std::ofstream os(file_name, std::ios::binary);
 		cereal::BinaryOutputArchive output(os);
-		output(m_time_series,m_dates);
+		output(m_time_series, m_map_dates, m_dates);
 	}
 
 	/* Load data structure from a binary file
@@ -104,7 +113,7 @@ public:
 	void load_data(const std::string &file_name) {
 		std::ifstream is(file_name, std::ios::binary);
 		cereal::BinaryInputArchive input(is);
-		input(m_time_series,m_dates);
+		input(m_time_series, m_map_dates, m_dates);
 	}
 
 	/* Return a vector with all the counters of page
@@ -117,12 +126,9 @@ public:
 	inline std::vector<uint32_t> range(
 		const std::string &page, uint32_t time1, uint32_t time2) const {
 
-		const auto lt_idx = std::lower_bound(m_dates.begin(), m_dates.end(), time1);
-		const auto rt_idx = std::lower_bound(lt_idx, m_dates.end(), time2);
 		const auto pg = m_time_series.at(page);
-
-		const auto lt_end = lt_idx - m_dates.begin();
-		const auto rt_end = rt_idx - m_dates.begin();
+		const auto lt_end = m_map_dates.at(time1);
+		const auto rt_end = m_map_dates.at(time2);
 
 		std::vector<uint32_t> result;
 		result.reserve(rt_end - lt_end + 1);
@@ -144,12 +150,9 @@ public:
 	inline std::vector<std::pair<uint32_t, uint32_t>> rangeTopK(
 		const std::string &page, uint32_t time1, uint32_t time2, uint32_t k) const {
 
-		const auto lt_idx = std::lower_bound(m_dates.begin(), m_dates.end(), time1);
-		const auto rt_idx = std::lower_bound(lt_idx, m_dates.end(), time2);
 		const auto pg = m_time_series.at(page);
-
-		const auto lt_end = lt_idx - m_dates.begin();
-		const auto rt_end = rt_idx - m_dates.begin();
+		const auto lt_end = m_map_dates.at(time1);
+		const auto rt_end = m_map_dates.at(time2);
 
 		auto compare = [](const std::pair<uint32_t, uint32_t> &p1,
 					      const std::pair<uint32_t, uint32_t> &p2) -> bool { return p1.second > p2.second; };
