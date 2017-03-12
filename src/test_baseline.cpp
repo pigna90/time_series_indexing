47033f5060d7d4c3d2d4dd13fbcf3d7a8bc7d3df
Alessandro <alerom90@gmail.com>
2017-03-12 16:25:27 +0100

    Baseline topk unit test

--- src/test_baseline.cpp
+++ src/test_baseline.cpp
@@ -7,10 +7,12 @@
 
 Baseline b;
 
-std::vector<range_case> cases;
+std::vector<range_case> range_cases;
+std::vector<topk_case> topk_cases;
 
-TEST(Range, Baseline){
-	for(auto const &c : cases){
+/*Baseline Range unit test*/
+TEST(Baseline, Range){
+	for(auto const &c : range_cases){
 		std::vector<uint32_t> res = b.range(c.page, c.date_begin, c.date_end);
 
 		ASSERT_EQ(res.size(), c.results.size());
@@ -21,13 +23,30 @@ TEST(Range, Baseline){
 	}
 }
 
+/*Baseline Topk unit test*/
+TEST(Baseline, TopK){
+	for(auto const &c : topk_cases){
+		std::vector<std::pair<uint32_t, uint32_t>> res = b.rangeTopK(c.page, c.date_begin, c.date_end, c.k);
+
+		ASSERT_EQ(res.size(), c.results.size());
+
+		for(size_t idx=0; idx<res.size(); ++idx){
+			EXPECT_EQ(res[idx].first, c.results[idx].first);
+			EXPECT_EQ(res[idx].second, c.results[idx].second);
+		}
+	}
+}
+
 int main(int argc, char** argv) {
-	if(argc == 3){
+	if(argc == 4){
 		testing::InitGoogleTest(&argc, argv);
 
         std::string ds_filename = argv[1];		//Dataset filename
-        std::string test_filename = argv[2];	//Queries and results filename
-        cases = load_range_cases(test_filename);
+        std::string range_filename = argv[2];	//Range queries and results filename
+        std::string topk_filename = argv[3];	//Topk queries and results filename
+
+		range_cases = load_range_cases(range_filename);
+        topk_cases = load_topk_cases(topk_filename);
 
 		b = Baseline(ds_filename);
 
