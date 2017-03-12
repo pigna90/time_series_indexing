e9724ee6c51589fbc993c5afd6aec81abe3dd98f
Alessandro <alerom90@gmail.com>
2017-03-12 16:34:33 +0100

    Function that load topk queries/results for unit test

--- src/test_common.hpp
+++ src/test_common.hpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <iostream>
 
+/* Struct for range query/result test case */
 struct range_case{
     std::string page;
     uint32_t date_begin;
@@ -11,11 +12,25 @@ struct range_case{
     std::vector<uint32_t> results;
 };
 
+/* Struct for topk query/result test case */
+struct topk_case{
+	std::string page;
+    uint32_t date_begin;
+    uint32_t date_end;
+    uint16_t k;
+
+    std::vector<std::pair<uint32_t, uint32_t>> results;
+};
+
+/* Loads range cases from file
+ *
+ * cases_filename: file name that contains cases for range
+ */
 std::vector<range_case> load_range_cases(const std::string cases_filename){
     std::ifstream infile(cases_filename);
     std::string token;
 
-	std::vector<range_case> cases_out;
+	std::vector<range_case> cases;
 
     for(std::string line; getline(infile, line); ) {
 		if(line.find("#") == std::string::npos){
@@ -36,8 +51,49 @@ std::vector<range_case> load_range_cases(const std::string cases_filename){
 			val = stoi(line.substr(0, line.find("\n")));
 			c.results.push_back(val);
 
-			cases_out.push_back(c);
+			cases.push_back(c);
 		}
     }
-    return cases_out;
+    return cases;
+}
+
+/* Loads topk cases from file
+ *
+ * cases_filename: file name that contains cases for topk
+ */
+std::vector<topk_case> load_topk_cases(const std::string cases_filename){
+	std::ifstream infile(cases_filename);
+    std::string token;
+
+	std::vector<topk_case> cases;
+
+	for(std::string line; getline(infile, line); ) {
+		if(line.find("#") == std::string::npos){
+			topk_case c;
+			c.page = line.substr(0, line.find("\t"));
+			line = line.substr(line.find("\t")+1, line.size());
+			c.date_begin = stoi(line.substr(0, line.find("\t")));
+			line = line.substr(line.find("\t")+1, line.size());
+			c.date_end = stoi(line.substr(0, line.find("\t")));
+			line = line.substr(line.find("\t")+1, line.size());
+			c.k = stoi(line.substr(0, line.find("/")));
+			line = line.substr(line.find("/")+1, line.size());
+
+			uint32_t time,val;
+			while(line.find("\t") != std::string::npos){
+				time = stoi(line.substr(0, line.find(",")));
+				line = line.substr(line.find(",")+1, line.size());
+				val = stoi(line.substr(0, line.find("\t")));
+				line = line.substr(line.find("\t")+1, line.size());
+				c.results.emplace_back(time, val);
+			}
+			time = stoi(line.substr(0, line.find(",")));
+			line = line.substr(line.find(",")+1, line.size());
+			val = stoi(line.substr(0, line.find("\n")));
+			c.results.emplace_back(time, val);
+
+			cases.push_back(c);
+		}
+	}
+	return cases;
 }
