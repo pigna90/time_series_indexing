17a0faf345467bf3ffdd421d237e9589ee2b72bb
Alessandro <alerom90@gmail.com>
2017-03-24 18:35:10 +0100

    Binary directory as parameter

--- analysis/run_all_queries.sh
+++ analysis/run_all_queries.sh
@@ -1,12 +1,15 @@
 ##
 # Runs all queries in the folder passed as argument
 ##
+# arg1 - path to queries files
+# arg2 - path to run_queries binary
+##
 
 echo "Baseline"
 for f in `ls -v $1/*.queries.*`
 do
     echo $f
-    ../_build/bin/run_queries 0 ../data/time_series.txt $f
+    $2/run_queries 0 ../data/time_series.txt $f
 done
 
 echo "-----"
@@ -15,5 +18,5 @@ echo "Index1"
 for f in `ls -v $1/*.queries.*`
 do
     echo $f
-    ../_build/bin/run_queries 1 ../data/time_series.txt $f
+    $2/run_queries 1 ../data/time_series.txt $f
 done
